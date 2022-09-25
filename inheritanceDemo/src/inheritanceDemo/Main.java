5faad3cb8d82b86d1f01b614b7ecc91f18f2b434
ahmetmete.2477 <ahmetmete.2477@gmail.com>
2022-09-25 20:29:14 +0300

    ders2 odev2

---
+++ inheritanceDemo/src/inheritanceDemo/Main.java
@@ -0,0 +1,13 @@
+package inheritanceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Course 37 Inheritance Demo	
+		
+		KrediUI krediUI = new KrediUI();
+		krediUI.KrediHesapla(new AskerKrediManager());
+		
+	}
+
+}
