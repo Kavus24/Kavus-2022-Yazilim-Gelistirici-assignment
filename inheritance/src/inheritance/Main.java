9970ffd6be10c7e710c8a0f0dabf7e7d4846a1ed
ahmetmete.2477 <ahmetmete.2477@gmail.com>
2022-09-25 20:09:59 +0300

    odev2 ders2

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Course 36 Inheritance 
+		
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+		EmployeeManager employeeManager = new EmployeeManager();
+		CustomerManager customerManager = new CustomerManager();
+		
+		
+	}
+
+}
